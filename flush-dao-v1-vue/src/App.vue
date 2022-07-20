facdc3a8dfe4ed44d94f352e183399d384090fe1
Antonio Ponce <zponce@posteo.net>
2022-07-19 22:43:48 -0400

    Created vue app 4 dao

---
+++ flush-dao-v1-vue/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1 class="special"> FLUSH DAO IS THE GOAT </h1>
+    <p><i class="special2">fr fr np cap</i></p>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.special {
+  color: green;
+  font-size: 10rem;
+}
+
+.special2 {
+ color: lightgreen;
+ font-size: 5rem;
+}
+
+@media (min-width: 1024px) {
+
+  header {
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: center;
+    flex-wrap: wrap;
+  }
+}
+</style>
